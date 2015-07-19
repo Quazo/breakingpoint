d866fcf60de9530c9cb12e9fa85973792f76658d
quazo <contact@johannesfranz.com>
2015-07-19 22:32:28 +0200

    Default Framerange, removed unnecessary config files

--- houdini/setup/scripts/456.py
+++ houdini/setup/scripts/456.py
@@ -8,16 +8,23 @@ import getpass
 import hou
 import platform
 
-USER = getpass.getuser()
-
+# BP Scene Defaults
 FPS = 25
+
+FSTART = 950
+FEND = 1100
+RFSTART = 1021
+RFEND = 1100
+
+USER = getpass.getuser()
+HIPNAME = hou.expandString('$HIPNAME')
 
 # Setting platform dependend basepath
 BP_BASE_PATH = ''
-if(platform.system()=='Windows'):
-	BP_BASE_PATH = '//bigfoot/breakingpoint'
+if(platform.system() == 'Windows'):
+    BP_BASE_PATH = '//bigfoot/breakingpoint'
 else:
-	BP_BASE_PATH = '/bigfoot/breakingpoint'
+    BP_BASE_PATH = '/bigfoot/breakingpoint'
 
 BP_PIPELINE_PATH = BP_BASE_PATH + '/_pipeline'
 BP_OTL_PATH = BP_PIPELINE_PATH + '/otl/publish'
@@ -34,42 +41,46 @@ BP_SHOTS_PATH = '{0}/2_production/2_shots'.format(BP_BASE_PATH)
 BP_SHOT_NAME = '$BP_SHOT_NAME'
 
 try:
-
-	hou.hscriptExpression(BP_SHOT_NAME)
-
-	BP_SHOT_NAME_VALUE = hou.expandString('$BP_SHOT_NAME')
+    hou.hscriptExpression(BP_SHOT_NAME)
+    BP_SHOT_NAME_VALUE = hou.expandString('$BP_SHOT_NAME')
 
 except:
-
-	print('Initializing {0} to an empty value'.format(BP_SHOT_NAME))
-
-	hou.hscript('set -g BP_SHOT_NAME = {0}'.format(''))
+    hou.hscript('set -g BP_SHOT_NAME = {0}'.format(''))
+    print('Initialized {0} to an empty value'.format(BP_SHOT_NAME))
 
 
-# Set variables
-# --------------------------------------------------------------------------------------------
+# Set Environment
+# -----------------------------------------------------------------------------
+
 try:
-	
-	
-	hou.hscript('set -g BP_SHOT_NAME = {0}'.format(BP_SHOT_NAME))
-	hou.hscript('set -g BP_SHOTS_PATH = {0}'.format(BP_SHOTS_PATH))
-	hou.hscript('set -g BP_HDRI_PATH = {0}'.format(BP_HDRI_PATH))
-	hou.hscript('set -g BP_OTL_PATH = {0}'.format(BP_OTL_PATH))
-	hou.hscript('set -g BP_RENDER_PATH = {0}'.format(BP_RENDER_PATH))
-	hou.hscript('set -g JOB = {0}'.format(BP_SCRIPTS_PATH))
+    hou.hscript('set -g BP_SHOT_NAME = {0}'.format(BP_SHOT_NAME))
+    hou.hscript('set -g BP_SHOTS_PATH = {0}'.format(BP_SHOTS_PATH))
+    hou.hscript('set -g BP_HDRI_PATH = {0}'.format(BP_HDRI_PATH))
+    hou.hscript('set -g BP_OTL_PATH = {0}'.format(BP_OTL_PATH))
+    hou.hscript('set -g BP_RENDER_PATH = {0}'.format(BP_RENDER_PATH))
+    hou.hscript('set -g JOB = {0}'.format(BP_SCRIPTS_PATH))
 
-	print 'Environment variables set.'
+    print('Environment variables set.')
 
-	
 except:
+    print("Could not set environment variables.")
 
-	print 'Could not set environment variables.'
 
+# Set Scene Values
+# -----------------------------------------------------------------------------
 
 # FPS
 hou.setFps(FPS)
-print('fps set to {0}'.format(FPS))
+print("FPS set to {0}".format(FPS))
 
+# Set Framerange
+if(HIPNAME == 'untitled'):
+    hou.hscript('tset `{0}/$FPS` `{1}/$FPS`'.format(FSTART, FEND))
+    hou.playbar.setPlaybackRange(RFSTART, RFEND)
+    hou.playbar.setRealTime(True)
+    hou.setFrame(RFSTART)
+    print("Default Framerange set.")
 
-print "456.cmd successfully executed."
-print "Welcome " + USER
+# Success messages
+print("456.cmd successfully executed.")
+print("Welcome " + USER)
--- houdini/start_houdini_sandbox.bat
+++ houdini/start_houdini_sandbox.bat
@@ -11,12 +11,14 @@ set "HOUDINI_VERSION=Houdini 14.0.361"
 set "RR_ROOT=//smaug/Renderfarm/_RR6"
 
  rem Houdini Variables
-set "PATH=%PLUGINS_PATH%/htoa-1.5.3_r1364_houdini-14.0.361/htoa-1.5.3_r1364_houdini-14.0.361/scripts/bin"
+set "PATH=%PLUGINS_PATH%/htoa-1.5.3_r1364_houdini-14.0.361/htoa-1.5.3_r1364_houdini-14.0.361/scripts/bin;%PATH%"
 set "HOUDINI_PATH=%PIPELINE_PATH%/_sandbox/houdini/setup;%PLUGINS_PATH%/htoa-1.5.3_r1364_houdini-14.0.361/htoa-1.5.3_r1364_houdini-14.0.361;&"
-set "HOUDINI_OTLSCAN_PATH=%PLUGINS_PATH%/qLib-dev/otls;%PIPELINE_PATH%/houdini/otl/publish;&"
-set "JOB=%PIPELINE_PATH%/_sandbox/houdini"
+ rem set "HOUDINI_SCRIPT_PATH=%PIPELINE_PATH%/_sandbox/houdini/setup/scripts"
+set "HOUDINI_OTLSCAN_PATH=%PLUGINS_PATH%/qLib-dev/otls/base;%PLUGINS_PATH%/qLib-dev/otls/experimental;%PLUGINS_PATH%/qLib-dev/otls/base/future;%PIPELINE_PATH%/houdini/otl/publish;&"
+ rem set "JOB=%PIPELINE_PATH%/_sandbox/houdini"
 set "HOUDINI_BUFFEREDSAVE=1"
- rem set "HOUDINI_USER_PREF_DIR=%PIPELINE_PATH%/_sandbox/houdini/setup/pref__HVER__"
+set "HOUDINI_USER_PREF_DIR=%PIPELINE_PATH%/_sandbox/houdini/setup/pref__HVER__"
+set "HOUDINI_TEMP_DIR=%PIPELINE_PATH%/houdini/temp_dir"
 
  rem Arnold Variables
 set "HOUDINI_DSO_ERROR=2" 
@@ -24,7 +26,7 @@ set "HTOA_STARTUP_LOG=0"
 
  rem SplashScreen
 set "HOUDINI_SPLASH_FILE=%PIPELINE_PATH%/img/logo/houdinisplash_sandbox.png"
-set "HOUDINI_SPLASH_MESSAGE=BP SANDBOX HOUDINI"
+set "HOUDINI_SPLASH_MESSAGE=BP SANDBOX - %HOUDINI_VERSION%"
 
  rem Call Houdini
 set "HOUDINI=C:/Program Files/Side Effects Software/%HOUDINI_VERSION%/bin"
